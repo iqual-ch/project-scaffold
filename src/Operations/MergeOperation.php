c0876d553508641c57716805b304b96309fd06d4
martinstadelmann <m.stadelmann@iqual.ch>
2023-09-07 11:31:23 +0000

    Fixed error if file destination doesn't exist

--- src/Operations/MergeOperation.php
+++ src/Operations/MergeOperation.php
@@ -158,12 +158,18 @@ class MergeOperation extends AbstractOperation {
       $io->write($interpolator->interpolate("  - Skipping <info>[dest-full-path]</info> because source is empty."), TRUE, 4);
       return new ScaffoldResult($destination, FALSE);
     }
-    // Copy the new contents to the destination.
+    // If there is content try to write the content.
     else {
-      // Make sure the directory exists.
-      $fs->ensureDirectoryExists(dirname($destination_path));
-      // Copy the new contents to the destination.
-      return $this->copyScaffold($destination, $io, $new_contents);
+      try {
+        // Make sure the directory exists.
+        $fs->ensureDirectoryExists(dirname($destination_path));
+        // Copy the new contents to the destination.
+        return $this->copyScaffold($destination, $io, $new_contents);
+      }
+      catch (\Exception $e) {
+        $io->write($interpolator->interpolate("  - Could not write <info>[dest-full-path]</info>: <warning>" . $e->getMessage() . "</warning>"));
+        return new ScaffoldResult($destination, FALSE);
+      }
     }
   }
 
